93456235abc96d688e7a15afeb3ee13cb20299fb
Codeflour <cynthianuchepro@gmail.com>
2020-04-22 23:25:51 +0100

    on it

--- pythonTask3.py
+++ pythonTask3.py
@@ -1,4 +1,26 @@
-#easy
+print (input("""
+Welcome to a guessing game!
+Choose yourlevel:
+    >Easy
+    >Medium  
+    >Hard
+
+"""))
+
+while input.lower != "easy" or input.lower !="medium" or input.lower !="hard":
+  if input.lower == easy:
+    easy()
+    break
+  else if input.lower == medium:
+    medium()
+    break
+  else if input.lower === hard:
+    hard()
+    break
+  else:
+    print("Please, choose a valid level.")
+
+def easy():
 secret_number=7
 guess_count=0
 guess_limit=6
@@ -20,7 +42,9 @@ while guess_count<guess_limit:
     print( "You have "+str(guess_remainder)+" more guesses."  )
 print("Game Over")
 
-#medium
+#easy()
+
+def medium():
 secret_number=7
 guess_count=0
 guess_limit=4
@@ -41,7 +65,9 @@ while guess_count<guess_limit:
     print( "You have "+str(guess_remainder)+" more guesses."  )
 print("Game Over")
 
-#hard
+#medium()
+
+def hard():
 secret_number=7
 guess_count=0
 guess_limit=3
@@ -61,3 +87,5 @@ while guess_count<guess_limit:
     print("That was wrong")
     print( "You have "+str(guess_remainder)+" more guesses."  )
 print("Game Over")
+
+#hard()
