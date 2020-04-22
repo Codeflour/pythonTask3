b3579653745091384db5a01a3dca1f70216bd601
Codeflour <cynthianuchepro@gmail.com>
2020-04-22 11:27:17 +0100

    skeleton

--- pythonTask3.py
+++ pythonTask3.py
@@ -1,8 +1,51 @@
+#easy
+secret_number=7
+guess_count=0
+guess_limit=6
+guess_remainder=(guess_limit-guess_count)
+
+while guess_count<guess_limit:
+  guess=int(input("Guess: "))
+  guess_count += 1
+  
+  #if guess != int:
+   # print ("Please, enter a number!")
+  if guess==secret_number:
+    print ("You got it right!")
+    break
+   
+  else:
+    guess_remainder -= 1
+    print("That was wrong")
+    print( "You have "+str(guess_remainder)+" more guesses."  )
+print("Game Over")
+
+#medium
+secret_number=7
+guess_count=0
+guess_limit=4
+guess_remainder=(guess_limit-guess_count)
+while guess_count<guess_limit:
+  guess=int(input("Guess: "))
+  guess_count += 1
+  
+  #if guess != int:
+   # print ("Please, enter a number!")
+  if guess==secret_number:
+    print ("You got it right!")
+    break
+   
+  else:
+    guess_remainder -= 1
+    print("That was wrong")
+    print( "You have "+str(guess_remainder)+" more guesses."  )
+print("Game Over")
+
+#hard
 secret_number=7
 guess_count=0
 guess_limit=3
 guess_remainder=(guess_limit-guess_count)
-#easy
 while guess_count<guess_limit:
   guess=int(input("Guess: "))
   guess_count += 1
