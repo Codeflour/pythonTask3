e98aef47898c349f0c65e1aa08d0938775fa12de
Codeflour <cynthianuchepro@gmail.com>
2020-04-24 11:09:47 +0100

    This was quite interesting

--- pythonTask3.py
+++ pythonTask3.py
@@ -5,23 +5,37 @@ def easy():
   guess_count=0
   guess_limit=6
   guess_remainder=(guess_limit-guess_count)
-
-  while guess_count<guess_limit:
-    guess=int(input("Guess: "))
-    guess_count += 1
+  guess_range= 10
   
-    #if guess != int:
-     # print ("Please, enter a number!")
-    if guess==secret_number:
-      print ("You got it right!")
-      welcome()
-      break
-     
-    else:
-      guess_remainder -= 1
-      print("That was wrong")
-      print( "You have "+str(guess_remainder)+" more guesses."  )
+  
     
+  while guess_count<guess_limit:
+   
+    
+    try:
+      guess=int(input("Guess a number from 1-10: "))
+      guess_count += 1
+      
+      if guess <= guess_range and guess >0:
+  
+     
+        if guess==secret_number:
+          print ("You got it right!")
+          continueWelcome()
+          break
+     
+        else:
+          guess_remainder -= 1
+          print("That was wrong")
+          print( "You have "+str(guess_remainder)+" more guesses."  )
+      
+      else:
+         print ("Your guess is out of range!")
+   
+   
+    except ValueError:
+      print ("Please, enter a number")
+   
   
   print("Game Over")
   
@@ -32,49 +46,129 @@ def medium():
   guess_count=0
   guess_limit=4
   guess_remainder=(guess_limit-guess_count)
-  while guess_count<guess_limit:
-    guess=int(input("Guess: "))
-    guess_count += 1
+  guess_range= 20
   
-    #if guess != int:
-     # print ("Please, enter a number!")
-    if guess==secret_number:
-      print ("You got it right!")
-      welcome()
-      break
+  
+    
+  while guess_count<guess_limit:
    
-    else:
-      guess_remainder -= 1
-      print("That was wrong")
-      print( "You have "+str(guess_remainder)+" more guesses."  )
+    
+    try:
+      guess=int(input("Guess a number from 1-20: "))
+      guess_count += 1
+      
+      if guess <= guess_range and guess >0:
+  
+     
+        if guess==secret_number:
+          print ("You got it right!")
+          continueWelcome()
+          break
+     
+        else:
+          guess_remainder -= 1
+          print("That was wrong")
+          print( "You have "+str(guess_remainder)+" more guesses."  )
+      
+      else:
+         print ("Your guess is out of range!")
+   
+   
+    except ValueError:
+      print ("Please, enter a number")
+   
+  
   print("Game Over")
 
+
 #medium()
+
 
 def hard():
   secret_number=35
   guess_count=0
   guess_limit=3
   guess_remainder=(guess_limit-guess_count)
-  while guess_count<guess_limit:
-    guess=int(input("Guess: "))
-    guess_count += 1
+  guess_range= 50
   
-    #if guess != int:
-     # print ("Please, enter a number!")
-    if guess==secret_number:
-      print ("You got it right!")
-      welcome()
-      break
+  
+    
+  while guess_count<guess_limit:
    
-    else:
-      guess_remainder -= 1
-      print("That was wrong")
-      print( "You have "+str(guess_remainder)+" more guesses."  )
+    
+    try:
+      guess=int(input("Guess a number from 1-50: "))
+      guess_count += 1
+      
+      if guess <= guess_range and guess >0:
+  
+     
+        if guess==secret_number:
+          print ("You got it right!")
+          continueWelcome()
+          break
+     
+        else:
+          guess_remainder -= 1
+          print("That was wrong")
+          print( "You have "+str(guess_remainder)+" more guesses."  )
+      
+      else:
+         print ("Your guess is out of range!")
+   
+   
+    except ValueError:
+      print ("Please, enter a number")
+   
+  
   print("Game Over")
-
+  
 #hard()
 
+
+#If the user finishes playing a level,and still to continue.
+
+def continueWelcome():
+  print ("""
+You've finished the current level!
+Choose another level:
+    >Easy
+    >Medium  
+    >Hard 
+    
+ or quit
+""")
+  #level =""
+  chosen_level = input().lower()
+  levels = ["easy", "medium", "hard", "quit"]
+
+  for level in levels:
+    #print (level)
+    if chosen_level == "easy":
+      easy()
+      break
+      
+    elif chosen_level == "medium":
+      medium()
+      break
+      
+    elif chosen_level == "hard":
+      hard()
+      break
+      
+    elif chosen_level == "quit":
+      break
+      #print("Game over!")
+      
+    else:
+      print("Please, choose a valid level.")
+      continueWelcome()
+    
+
+
+
+
+#The welcome function
 def welcome():
   print ("""
 Welcome to a guessing game!
@@ -85,20 +179,20 @@ Choose yourlevel:
 
 """)
   #level =""
-  level = input().lower()
+  chosen_level = input().lower()
   levels = ["easy", "medium", "hard"]
 
-  for lvl in levels:
+  for level in levels:
     #print (level)
-    if level == "easy":
+    if chosen_level == "easy":
       easy()
       break
       
-    elif level == "medium":
+    elif chosen_level == "medium":
       medium()
       break
       
-    elif level == "hard":
+    elif chosen_level == "hard":
       hard()
       break
     else:
